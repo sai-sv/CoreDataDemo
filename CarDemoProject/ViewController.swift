5f9032f1433a1331e7827734adfda568ceca5f76
sergei <sergei.sai@bk.ru>
2019-12-09 22:04:29 +0300

    Initial Commit

---
+++ CarDemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarDemoProject
+//
+//  Created by Admin on 09.12.2019.
+//  Copyright © 2019 sergei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
